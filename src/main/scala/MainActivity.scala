32de1550ed5bf4b195b809680fa06449a8608244
pcboy <david.hagege@gmail.com>
2013-10-14 02:12:43 +0900

    first commit

---
+++ src/main/scala/MainActivity.scala
@@ -0,0 +1,12 @@
+package com.scratch
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity extends Activity with TypedActivity {
+  override def onCreate(bundle: Bundle) {
+    super.onCreate(bundle)
+    setContentView(R.layout.main)
+    findView(TR.textview).setText("hello, world!")
+  }
+}
